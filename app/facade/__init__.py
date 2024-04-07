66da17a774683e56b45eaf4a303d6a704fa812e3
ztyangt <yang2210670@163.com>
2024-04-07 22:48:41 +0800

    【feat✨】异常处理

--- app/facade/__init__.py
+++ app/facade/__init__.py
@@ -1,3 +1,18 @@
+from fastapi import FastAPI
 from .response import Response as RES
 from .logger import Loggers
 from .logger import log
+from .exception import validation_exception_handler, http_exception_handler, app_exception_handler
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+
+def registerCustomErrorHandle(server: FastAPI):
+    """ 统一注册自定义错误处理器 """
+    server.add_exception_handler(
+        RequestValidationError, validation_exception_handler)
+    # 错误处理StarletteHTTPException
+    server.add_exception_handler(
+        StarletteHTTPException, http_exception_handler)
+    # 自定义全局系统错误
+    server.add_exception_handler(Exception, app_exception_handler)
--- app/facade/exception.py
+++ app/facade/exception.py
@@ -1,6 +1,10 @@
+import traceback
+from app.facade import log
 from app.facade import RES
 from functools import wraps
-from app.facade import log
+from fastapi import status
+from fastapi.requests import Request
+from starlette.exceptions import HTTPException
 
 
 async def validation_exception_handler(request, exc):
@@ -23,5 +27,22 @@ def handle_api_exceptions(func):
         except Exception as e:
             log.error(f"接口异常: {e}")
             return RES.res_400(str(e))
-
     return wrapper
+
+
+async def http_exception_handler(request, exc: HTTPException):
+    """自定义处理HTTPException"""
+    if exc.status_code == status.HTTP_404_NOT_FOUND:
+        # 处理404错误
+        return RES.res_200(code=404, msg="接口路由不存在！")
+    elif exc.status_code == status.HTTP_405_METHOD_NOT_ALLOWED:
+        # 处理405错误
+        return RES.res_200(code=404, msg="请求方式错误！")
+    else:
+        return RES.res_200(code=400, msg="未知错误！")
+
+
+async def app_exception_handler(request: Request, exc: Exception):
+    """自定义全局系统错误"""
+    log.error(f"系统异常: {traceback.format_exc()}")
+    return RES.res_200(code=500, msg="系统异常,请稍后重试~")
